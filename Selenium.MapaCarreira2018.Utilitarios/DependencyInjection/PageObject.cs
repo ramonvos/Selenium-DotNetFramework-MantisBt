ba90593a36b5e8e6004ac7c42ff8e84cfccfe329
Ramon Vinicius <ramon.souza1988@gmail.com>
2018-08-30 22:56:36 -0300

    Create project

--- Selenium.MapaCarreira2018.Utilitarios/DependencyInjection/PageObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Selenium.MapaCarreira2018.Utilitarios.DependencyInjection
-{
-    //[AttributeUsage(AttributeTargets.Field)]
-    //public class PageObject : Attribute { }
-}
